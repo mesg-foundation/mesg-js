95098c0961c09f4d19c05316dd8815ae1ecd3cc5
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-10-16 20:30:40 +0300

    add test for starting same services only once, cleanup code and improve changelog

--- src/application/application.ts
+++ src/application/application.ts
@@ -17,7 +17,7 @@ class Application {
     api: CoreClient
 
     private client: CoreClient
-    private serviceIds: string[] = []
+    private serviceIDs: string[] = []
 
     constructor(options: Options){
         this.client = options.client;
@@ -82,14 +82,16 @@ class Application {
     }
 
     private async startService(id: string) {
+        // service is already starting.
+        if (this.serviceIDs.indexOf(id) >= 0) {
+            return
+        }
+        this.serviceIDs = [...this.serviceIDs, id];
+
         try {
-            // Service is already starting
-            if (this.serviceIds.indexOf(id) < 0) {
-                this.serviceIds = [...this.serviceIds, id];
-                await new Promise<StartServiceReply | Error>((resolve, reject) => {
-                    this.client.startService({ serviceID: id }, handleAPIResponse(resolve, reject));
-                });
-            }
+            await new Promise<StartServiceReply | Error>((resolve, reject) => {
+                this.client.startService({ serviceID: id }, handleAPIResponse(resolve, reject));
+            });
         } catch (e) {
             throw new Error(`Error while starting service ${e}`)
         }
