223cb0763c1c50c5c96ef737d352e5e7fb41653c
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2019-01-10 00:16:32 +0300

    application: add wrappers for listen event/result & execute task

--- src/application/application.ts
+++ src/application/application.ts
@@ -11,6 +11,20 @@ class Application {
     constructor(options: Options){
         this.api = options.client;
     }
+
+    listenEvent(req: ListenEventRequest): Stream<EventData> {
+        return this.api.ListenEvent(req)
+    }
+
+    listenResult(req: ListenResultRequest): Stream<ResultData> {
+        return this.api.ListenResult(req)
+    }
+
+    executeTask(req: ExecuteTaskRequest): Promise<ExecuteTaskReply> {
+        return new Promise<ExecuteTaskReply>((resolve, reject) => {
+            this.api.ExecuteTask(req, handleAPIResponse(resolve, reject))
+        })
+    }
 }
 
 declare interface Stream<T> {
@@ -20,8 +34,51 @@ declare interface Stream<T> {
     on(event: 'status', listener: (status) => void): this;
 }
 
+interface ListenEventRequest {
+    serviceID: string
+    eventFilter?: string
+}
+
+interface ListenResultRequest {
+    serviceID: string
+    taskFilter?: string
+    outputFilter?: string
+    tagFilters?: string[]
+}
+
+interface EventData {
+    eventKey: string
+    eventData: string
+}
+
+interface ResultData {
+    executionID: string
+    taskKey: string
+    outputKey: string
+    outputData: string
+    executionTags: string[]
+    error: string
+}
+
+interface ExecuteTaskRequest {
+    serviceID: string
+    taskKey: string
+    inputData: string
+    executionTags?: string[]
+}
+
+interface ExecuteTaskReply {
+    executionID: string
+}
+
 export default Application;
 export {
     Options,
-    Stream
+    Stream,
+    ListenEventRequest,
+    ExecuteTaskRequest,
+    ExecuteTaskReply,
+    ListenResultRequest,
+    EventData,
+    ResultData
 }
