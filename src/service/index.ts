daee957704e84b161f234f3aed331d12126f0e23
Anthony ESTEBE <anthony@mesg.com>
2019-06-28 00:48:59 +0700

    update service with new api

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,19 +1,19 @@
 import * as YAML from 'js-yaml'
 import * as fs from 'fs'
 import Service from './service'
-import { createClient } from '../util/grpc'
+import API from '../api'
 
 const token = process.env.MESG_TOKEN
 const endpoint = process.env.MESG_ENDPOINT
 const ymlPath = './mesg.yml'
 
 const serviceBuilder = (): Service => {
-	const definition = YAML.safeLoad(fs.readFileSync(ymlPath));
-	return new Service({
-		token: token,
-		definition: definition,
-		client: createClient('Service', 'protobuf/serviceapi/api.proto', endpoint)
-	});
+  const definition = YAML.safeLoad(fs.readFileSync(ymlPath));
+  return new Service({
+    token: token,
+    definition: definition,
+    API: API(endpoint)
+  });
 }
 
 export default serviceBuilder;
@@ -25,8 +25,4 @@ export {
 export {
   Tasks,
   TaskInputs,
-  Stream,
-  EmitEventReply,
-  SubmitResultReply,
-  TaskData
 } from './service'
--- src/service/service_test.ts
+++ src/service/service_test.ts
@@ -1,66 +1,51 @@
 import * as test from 'tape'
 import * as sinon from 'sinon'
 import Service from './service'
-import { EventEmitter } from 'events';
-
-class testClient {
-  listenTask(): EventEmitter {
-    return new EventEmitter;
-  }
-  emitEvent(){}
-  submitResult(){}
-}
+import Api, { API } from '../api/mock'
 
 const token = "token"
 
-function newService(config: any, client: testClient): Service {
+function newService({
+  definition = {},
+  api = Api(''),
+}: { definition?: any, api?: API }): Service {
   return new Service({
-    token: token,
-    definition: config,
-    client: client
+    token,
+    definition,
+    API: api
   })
 }
 
-test('Service should expose the service gRPC api', (t) => {
-  t.plan(1);
-  const client = new testClient;
-  const definition = { tasks: {"task1": {}} };
-  const service = newService(definition, client);
-  t.ok(service.api);
-});
 
 test('listenTask() should pass task validation', function (t) {
   t.plan(1);
-  const client = new testClient;
-  const definition = { tasks: {"task1": {}, "task2": {}} };
-  const service = newService(definition, client);
-  const spy = sinon.stub(service, 'listenTask');
+  const definition = { tasks: { "task1": {}, "task2": {} } };
+  const service = newService({ definition });
+  sinon.stub(service, 'listenTask');
   try {
-    service.listenTask({ "task1": () => ({}),  "task2": () => ({}) });
+    service.listenTask({ "task1": () => ({}), "task2": () => ({}) });
     t.pass("tasks valid");
-  } catch(e) {
+  } catch (e) {
     t.error(e);
   }
 });
 
 test('listenTask() should throw because missing task in mesg.yml', function (t) {
   t.plan(1);
-  const client = new testClient;
-  const definition = { tasks: {"task1": {}} };
-  const service = newService(definition, client);
+  const definition = { tasks: { "task1": {} } };
+  const service = newService({ definition });
   try {
-    service.listenTask({ "task1": () => ({}),  "task2": () => ({}) });
+    service.listenTask({ "task1": () => ({}), "task2": () => ({}) });
     t.fail("should throw");
-  } catch(e) {
+  } catch (e) {
     t.ok(e);
   }
 });
 
 test('listenTask() should give warning because missing task callback', function (t) {
   t.plan(1);
-  const client = new testClient;
-  const definition = { tasks: {"task1": {}, "task2": {}} };
-  const service = newService(definition, client);
+  const definition = { tasks: { "task1": {}, "task2": {} } };
+  const service = newService({ definition });
   const spy = sinon.spy(console, 'warn');
   service.listenTask({ "task1": () => ({}) });
   t.equal(spy.getCall(0).args[0], 'WARNING: The following tasks described in the mesg.yml haven\'t been implemented: task2');
@@ -69,73 +54,73 @@ test('listenTask() should give warning because missing task callback', function 
 
 test('listenTask() should throw when called more than once', function (t) {
   t.plan(1);
-  const client = new testClient;
-  const definition = { tasks: {"task1": {}} };
-  const service = newService(definition, client);
+  const definition = { tasks: { "task1": {} } };
+  const service = newService({ definition });
   service.listenTask({ "task1": () => ({}) });
   try {
     service.listenTask({ "task1": () => ({}) });
     t.fail("should throw");
-  } catch(e) {
+  } catch (e) {
     t.ok(e);
   }
 });
 
 test('listenTask() should listen for tasks', function (t) {
   t.plan(2);
-  const client = new testClient;
-  const spy = sinon.spy(client, 'listenTask');
-  const definition = { tasks: {'task1': {}, 'task2': {}} };
-  const service = newService(definition, client);
+  const api = Api('');
+  const spy = sinon.spy(api.execution, 'Stream');
+  const definition = { tasks: { 'task1': {}, 'task2': {} } };
+  const service = newService({ definition, api });
   service.listenTask({ 'task1': () => ({}), 'task2': () => ({}) });
   t.ok(spy.calledOnce);
-  t.equal(spy.getCall(0).args[0].token, token);
+  t.equal(spy.getCall(0).args[0].filter.instanceHash, token);
   spy.restore();
 });
 
 test('listenTask() should handle tasks and submit result', async function (t) {
   t.plan(4);
-  const executionHash = 'hash';
-  const inputData = {input: 'data'};
-  const outputData = {output: 'data'};
-  const client = new testClient;
-  const spy = sinon.spy(client, 'submitResult');
-  const definition = { tasks: {'task1': { inputs: {}, outputs: {}}}};
-  const service = newService(definition, client);
-  const stream = <any>service.listenTask({ 'task1': (inputs) => {
-    t.deepEqual(inputData, inputs);
-    return outputData
-  }});
-  t.doesNotThrow(() => stream.emit('data', { executionHash, taskKey: 'task1', inputData: JSON.stringify(inputData) }));
-  await setTimeout(()=>{}, 0)
+  const hash = 'hash';
+  const inputs = { input: 'data' };
+  const outputs = { output: 'data' };
+  const api = Api('');
+  const spy = sinon.spy(api.execution, 'Update');
+  const definition = { tasks: { 'task1': { inputs: {}, outputs: {} } } };
+  const service = newService({ definition, api });
+  const stream = <any>service.listenTask({
+    'task1': (inputs) => {
+      t.deepEqual(inputs, inputs);
+      return outputs
+    }
+  });
+  t.doesNotThrow(() => stream.emit('data', { hash, taskKey: 'task1', inputs: JSON.stringify(inputs) }));
+  await setTimeout(() => { }, 0)
   const args = spy.getCall(0).args[0];
   spy.restore();
-  t.equal(args.executionHash, executionHash);
-  t.equal(args.outputData, JSON.stringify(outputData));
+  t.equal(args.hash, hash);
+  t.equal(args.outputs, JSON.stringify(outputs));
 });
 
 test('emitEvent() should emit an event', function (t) {
   t.plan(4);
-  const event = 'event1';
-  const eventData = {event: 'data'};
-  const client = new testClient;
-  const definition = {};
-  const service = newService(definition, client);
-  const spy = sinon.spy(client, 'emitEvent');
-  t.doesNotThrow(() => service.emitEvent(event, eventData));
+  const key = 'event1';
+  const data = { event: 'data' };
+  const api = Api('');
+  const spy = sinon.spy(api.event, 'Create');
+  const service = newService({ api });
+  t.doesNotThrow(() => service.emitEvent(key, data));
   const args = spy.getCall(0).args[0];
-  t.equal(args.token, token);
-  t.equal(args.eventKey, event);
-  t.equal(args.eventData, JSON.stringify(eventData));
+  t.equal(args.instanceHash, token);
+  t.equal(args.key, key);
+  t.equal(args.data, JSON.stringify(data));
   spy.restore();
 });
 
 test('emitEvent() should throw when no data provided', function (t) {
   t.plan(1);
-  const service = newService({}, new testClient);
+  const service = newService({});
   try {
     (<any>service.emitEvent)('event1')
-  } catch(e) {
+  } catch (e) {
     t.equal(e.message, 'data object must be send while emitting event')
   }
 });
