435231c5619576894efab399d35ffb87be7b649a
Anthony ESTEBE <anthony@mesg.com>
2019-06-28 14:29:35 +0700

    fix path of apis

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,10 +2,10 @@ import { createClient } from '../util/grpc'
 import { API } from './types'
 
 export default (endpoint: string): API => ({
-  event: createClient('Event', 'protobuf/event/api.proto', endpoint),
-  execution: createClient('Execution', 'protobuf/execution/api.proto', endpoint),
-  instance: createClient('Instance', 'protobuf/instance/api.proto', endpoint),
-  service: createClient('Service', 'protobuf/service/api.proto', endpoint),
+  event: createClient('Event', 'protobuf/api/event.proto', endpoint),
+  execution: createClient('Execution', 'protobuf/api/execution.proto', endpoint),
+  instance: createClient('Instance', 'protobuf/api/instance.proto', endpoint),
+  service: createClient('Service', 'protobuf/api/service.proto', endpoint),
 })
 
 export * from './types' 
