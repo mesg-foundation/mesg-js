f466ec2a8d62803fea8ca3e9f062e1ba527fc213
Anthony ESTEBE <anthony@mesg.com>
2019-07-04 20:16:58 +0700

    simplification of the resolver, resolve only by sid

--- src/util/resolve.ts
+++ src/util/resolve.ts
@@ -1,42 +1,21 @@
-import { hash, API, Instance } from "../api";
+import { hash, API } from "../api";
 
 const _resolutionTable: Map<string, hash> = new Map()
 
-// findInstanceFromService returns an instance of a service with the following strategy:
-//   - find a service that has the same hash than the serviceID
-//   - find a service that has the same sid than the serviceID (only if there is no match for the hash)
-//   - get all the instances of this service
-//   - return the instance of the service and throw an error if there is other than one instance running
-const findInstanceFromService = async (api: API, serviceID: string): Promise<Instance> => {
+// returns an instanceHash based on an sid
+// throw an error if the sid doesn't exists or have if it has more or less than one instance running
+export const resolveSID = async (api: API, sid: string): Promise<hash> => {
+  if (_resolutionTable.has(sid)) return _resolutionTable.get(sid)
+
   const { services } = await api.service.list({})
-  // Find by hash then by sid
-  const service = services.find(x => x.hash === serviceID) || services.find(x => x.sid === serviceID)
-  if (!service) throw new Error(`cannot resolve ${serviceID}`)
+  const service = services.find(x => x.sid === sid)
+  if (!service) throw new Error(`cannot resolve ${sid}`)
 
   // find matching instances
   const { instances } = await api.instance.list({ serviceHash: service.hash })
   if (!instances || instances.length === 0) throw new Error(`no instances running for the service ${service.sid}`)
   if (instances.length > 1) throw new Error(`multiple instances running for the service ${service.sid}`)
-  return instances[0]
-}
 
-// resolveInstanceHash
-// Return the instance hash based on a serviceID that can be:
-//   - instanceHash
-//   - sid
-//   - serviceHash
-// Throw an error if a serviceID has multiple instances running
-// Warning: this function is making multiple requests to the Engine so not really efficient.
-// it needs to be used carefully
-export const resolveInstanceHash = async (api: API, serviceID: string): Promise<hash> => {
-  if (_resolutionTable.has(serviceID)) return _resolutionTable.get(serviceID)
-
-  try {
-    const instance = await api.instance.get({ hash: serviceID })
-    _resolutionTable.set(serviceID, instance.hash)
-  } catch (e) {
-    const instance = await findInstanceFromService(api, serviceID)
-    _resolutionTable.set(serviceID, instance.hash)
-  }
-  return _resolutionTable.get(serviceID)
+  _resolutionTable.set(sid, instances[0].hash)
+  return _resolutionTable.get(sid)
 }
