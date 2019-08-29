60a9328ce663035a0b094b094a349bae90829952
Nicolas Mahe <nicolas@mahe.me>
2019-08-29 18:49:05 +0700

    use Uint8Array instead of Buffer in base58 encode function

--- src/util/base58.ts
+++ src/util/base58.ts
@@ -1,5 +1,5 @@
 const base = require('base-x')
 const bs58 = base('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz')
 
-export const decode: (string: string) => Buffer = bs58.decode
-export const encode: (source: Buffer) => string = bs58.encode
+export const decode: (value: string) => Buffer = bs58.decode
+export const encode = (value: Uint8Array): string => bs58.encode(Buffer.from(value))
